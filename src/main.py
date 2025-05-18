1f527499a40cf08ba4722e8ba7b0b05e61e89237
a.shilov <tabbols95@gmail.com>
2025-05-18 21:11:01 +0300

    first test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 def get_user() -> dict[str, any]:
     """Возвращает словарь с информацией о пользователе"""
-    return {"name": "Алёша Попович", "age": 30}
+    return {"name": "Алёша Попович", "age": 30, "gender": "m"}
 
 
 if __name__ == "__main__":
